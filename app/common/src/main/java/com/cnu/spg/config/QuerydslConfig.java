622b166102688c6e5cac8f3b0c294d6e1db8024c
kihwankim <clack2933@gmail.com>
2021-07-22 19:58:04 +0900

    fix: base entity JPA Auth 설정

--- app/common/src/main/java/com/cnu/spg/config/QuerydslConfig.java
+++ app/common/src/main/java/com/cnu/spg/config/QuerydslConfig.java
@@ -4,10 +4,12 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 
 @Configuration
+@EnableJpaAuditing
 @RequiredArgsConstructor
 public class QuerydslConfig {
 
