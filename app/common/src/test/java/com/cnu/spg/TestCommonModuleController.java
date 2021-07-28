57687abbcec1a7a15677a899d47209be1c2ec434
kihwankim <clack2933@gmail.com>
2021-07-28 19:48:05 +0900

    test: user domain 테스트 코드 추가

---
+++ app/common/src/test/java/com/cnu/spg/TestCommonModuleController.java
@@ -0,0 +1,11 @@
+package com.cnu.spg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommonModuleController {
+    @Test
+    void contextLoads() {
+    }
+}
