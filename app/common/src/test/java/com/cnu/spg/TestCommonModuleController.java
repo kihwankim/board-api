91b31f0ce4bc392a0ec724db28fe1a68096d9f0e
kihwankim <clack2933@gmail.com>
2021-07-29 23:25:49 +0900

    test: username을 통한 검색 테스트 코드 추가

--- app/common/src/test/java/com/cnu/spg/TestCommonModuleController.java
+++ app/common/src/test/java/com/cnu/spg/TestCommonModuleController.java
@@ -1,10 +1,26 @@
 package com.cnu.spg;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootApplication
+@TestConfiguration
 public class TestCommonModuleController {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
     @Test
     void contextLoads() {
     }
