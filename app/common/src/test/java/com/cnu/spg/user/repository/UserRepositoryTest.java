f4d3926e4cadc4de915c022d366a6b7ec79b5a5c
kihwankim <clack2933@gmail.com>
2021-07-30 19:55:12 +0900

    test: username exist 테스크 코드 추가

--- app/common/src/test/java/com/cnu/spg/user/repository/UserRepositoryTest.java
+++ app/common/src/test/java/com/cnu/spg/user/repository/UserRepositoryTest.java
@@ -10,8 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import javax.persistence.EntityManager;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("user db 처리 관련 테스트")
@@ -63,5 +62,40 @@ class UserRepositoryTest {
         // then
         assertThrows(UsernameNotFoundException.class, () -> userRepository.findByUsername(findedUsername)
                 .orElseThrow(() -> new UsernameNotFoundException("exception")));
+    }
+
+    @Test
+    public void deleteByUsername() throws Exception {
+        // given
+
+        // when
+        userRepository.deleteByUsername(username);
+
+        // then
+        assertThrows(UsernameNotFoundException.class, () -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("not found")));
+    }
+
+    @Test
+    void isNotExistUsernameTest() throws Exception {
+        // given
+        final String notExistUsername = "notExist";
+
+        // when
+        boolean result = userRepository.existsByUsername(notExistUsername);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    void isExistUsernameTest() throws Exception {
+        // given
+
+        // when
+        boolean result = userRepository.existsByUsername(username);
+
+        // then
+        assertTrue(result);
     }
 }
