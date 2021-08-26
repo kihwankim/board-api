5a53c85fcaefbf82e588bc3d83b0f7348a21127d
kihwankim <clack2933@gmail.com>
2021-08-26 19:15:49 +0900

    feat: swagger과 h2 console url을 security 검사에서 재외 시켜줌

--- app/api-service/src/main/java/com/cnu/spg/security/SecurityConfig.java
+++ app/api-service/src/main/java/com/cnu/spg/security/SecurityConfig.java
@@ -2,10 +2,20 @@ package com.cnu.spg.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "/swagger-ui.html/**"
+                );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
