c006609e42c1a16234151a18f42cb91f8dc14ba9
kihwankim <clack2933@gmail.com>
2021-09-14 20:38:21 +0900

    feat: security 기본 setting

--- app/api-service/src/main/java/com/cnu/spg/security/config/SecurityConfig.java
+++ app/api-service/src/main/java/com/cnu/spg/security/config/SecurityConfig.java
@@ -1,12 +1,21 @@
-package com.cnu.spg.security;
+package com.cnu.spg.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String LOGIN_URL_PATH = "/v1/login";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) {
@@ -20,8 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers(LOGIN_URL_PATH).permitAll()
+                .anyRequest().authenticated();
+
+        http
+                .csrf().disable();
     }
 }
--- app/api-service/src/main/java/com/cnu/spg/security/config/SecurityWebApplicationAdapter.java
+++ app/api-service/src/main/java/com/cnu/spg/security/config/SecurityWebApplicationAdapter.java
@@ -1,4 +1,4 @@
-package com.cnu.spg.security;
+package com.cnu.spg.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
