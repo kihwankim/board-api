24fc0de835102b8ff165425762965f403825043f
kihwankim <clack2933@gmail.com>
2021-09-15 20:30:08 +0900

    feat: login validator 추가

--- app/core-module/src/main/java/com/cnu/spg/user/exception/LoginRequestParamterNotValidException.java
+++ app/core-module/src/main/java/com/cnu/spg/user/exception/LoginRequestParamterNotValidException.java
@@ -1,9 +1,15 @@
 package com.cnu.spg.user.exception;
 
-public class LoginRequestParamterNotValidException extends RuntimeException {
+import org.springframework.security.core.AuthenticationException;
+
+public class LoginRequestParamterNotValidException extends AuthenticationException {
     private static final String PARAMTER_NOT_VALID_MSG = "login parameter is not valid";
 
     public LoginRequestParamterNotValidException() {
         super(PARAMTER_NOT_VALID_MSG);
+    }
+
+    public LoginRequestParamterNotValidException(String msg) {
+        super(msg);
     }
 }
