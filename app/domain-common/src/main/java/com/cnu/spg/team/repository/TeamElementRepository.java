72147103592e3d72a28686e0425c5cd4dfcca487
kihwankim <clack2933@gmail.com>
2021-09-20 02:43:46 +0900

    refactor: 사용하지 않는 객체 삭제

--- app/domain-common/src/main/java/com/cnu/spg/team/repository/TeamElementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cnu.spg.team.repository;
-
-import com.cnu.spg.team.domain.TeamElement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamElementRepository extends JpaRepository<TeamElement, Long>, TeamElementCustomRepository {
-}
