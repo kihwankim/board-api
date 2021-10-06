51c1fff60f18df0dc1c0657a9afcc6c753153e2c
kihwankim <clack2933@gmail.com>
2021-10-06 20:14:51 +0900

    refactor: querydsl config와 audit config 설정 분리

--- app/domain-common/src/main/java/com/cnu/spg/config/querydsl/QuerydslConfig.java
+++ app/domain-common/src/main/java/com/cnu/spg/config/querydsl/QuerydslConfig.java
@@ -4,12 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-@EnableJpaAuditing
 @RequiredArgsConstructor
 public class QuerydslConfig {
 
