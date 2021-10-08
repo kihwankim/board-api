dacc2dfe16eaa9c64ecbed82fcda30785d28031d
kihwankim <clack2933@gmail.com>
2021-10-08 18:02:53 +0900

    refactor: code smell 제거

--- app/api-service/src/main/java/com/cnu/spg/user/advice/UserAdviceController.java
+++ app/api-service/src/main/java/com/cnu/spg/user/advice/UserAdviceController.java
@@ -1,6 +1,5 @@
 package com.cnu.spg.user.advice;
 
-import com.cnu.spg.comon.exception.NotFoundException;
 import com.cnu.spg.user.exception.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +17,7 @@ public class UserAdviceController {
      * board 정보를 받을 수 없음
      */
     @ExceptionHandler({RoleNotFoundException.class, UserNotFoundException.class})
-    protected ResponseEntity<String> handleNotFoundException(final NotFoundException exception) {
+    protected ResponseEntity<String> handleNotFoundException(final RuntimeException exception) {
         log.error(exception.getMessage(), exception);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
