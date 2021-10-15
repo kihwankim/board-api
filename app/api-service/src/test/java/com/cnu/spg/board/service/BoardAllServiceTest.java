f03c63ceacc8e642547cb1d5b15ebf44cc7eeb1f
kihwankim <clack2933@gmail.com>
2021-10-15 22:44:30 +0900

    test: board 한개의 정보 조회 테스트 케이스 추가

--- app/api-service/src/test/java/com/cnu/spg/board/service/BoardAllServiceTest.java
+++ app/api-service/src/test/java/com/cnu/spg/board/service/BoardAllServiceTest.java
@@ -1,10 +1,14 @@
 package com.cnu.spg.board.service;
 
 import com.cnu.spg.board.domain.Board;
+import com.cnu.spg.board.domain.BoardType;
 import com.cnu.spg.board.domain.project.ProjectBoard;
 import com.cnu.spg.board.domain.project.ProjectCategory;
 import com.cnu.spg.board.dto.condition.BoardSearchCondition;
+import com.cnu.spg.board.dto.response.BoardDetailResponse;
 import com.cnu.spg.board.dto.response.BoardResponse;
+import com.cnu.spg.board.exception.BoardNotFoundException;
+import com.cnu.spg.board.exception.BoardTypeNotValidException;
 import com.cnu.spg.board.repository.BoardRepository;
 import com.cnu.spg.board.repository.project.ProjectCategoryRepository;
 import com.cnu.spg.user.domain.User;
@@ -20,6 +24,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -114,11 +119,41 @@ class BoardAllServiceTest {
     @DisplayName("board 정보 조회")
     void getOneBoardTest() {
         // given
+        User user = User.createUser("name", "john@gmail.com", "Abc123!@");
+        ProjectCategory category = ProjectCategory.builder()
+                .user(user)
+                .categoryName("category")
+                .build();
+        userRepository.save(user);
+        categoryRepository.save(category);
+        Board board = ProjectBoard.builder()
+                .projectCategory(category)
+                .content("content_1")
+                .title("title_1")
+                .user(user)
+                .build();
+        Long id = boardRepository.save(board).getId();
+        BoardType projectType = BoardType.PROJECT;
 
+        // when
+        BoardDetailResponse findBoard = boardAllService.getBoard(projectType, id);
+
+        // then
+        assertEquals("content_1", findBoard.getContent());
+        assertEquals("title_1", findBoard.getTitle());
+        assertEquals(id, findBoard.getId());
+        assertEquals(0, findBoard.getComments().size());
+        assertEquals("name", findBoard.getWriterName());
+    }
+
+    @Test
+    @DisplayName("board type 이 적절하지 않을 경우")
+    void boardTypeExceptionTest() throws Exception {
+        // given
 
         // when
 
         // then
-
+        assertThrows(BoardNotFoundException.class, () -> boardAllService.getBoard(null, 1L));
     }
 }
